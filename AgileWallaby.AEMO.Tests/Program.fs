deb7fe9acade161babce9ca3c3b4a749b534ca10
James Webster <jim@agilewallaby.net>
2020-02-18 16:24:42 +1000

    Remove Program.fs

--- AgileWallaby.AEMO.Tests/Program.fs
+++
@@ -1,8 +0,0 @@
-module AgileWallaby.AEMO.Tests.Main
-
-[<EntryPoint>]
-let main args =
-    //TODO: Replace with xunit.runner.utility
-    let r = 0 //runTestsInAssembly defaultConfig args
-    if System.Diagnostics.Debugger.IsAttached then System.Console.ReadLine() |> ignore
-    r
